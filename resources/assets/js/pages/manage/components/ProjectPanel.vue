cb3b22a4bf36d5a4dd6f2e52d1cf876b689c9d8e
kuaifan <aipaw@live.cn>
2025-03-03 12:51:33 +0800

    fix: 点击排序导致任务不显示的情况

--- resources/assets/js/pages/manage/components/ProjectPanel.vue
+++ resources/assets/js/pages/manage/components/ProjectPanel.vue
@@ -1310,6 +1310,7 @@ export default {
         onSort(field) {
             this.sortField = field;
             this.sortType = this.sortType == 'desc' ? 'asc' : 'desc';
+            this.handleColumnDebounce();
         },
 
         onSetting() {
